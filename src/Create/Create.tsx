4dbf21e1550bdf0c55c4d2e262dfb2f029fe500f
evangoertzen <evan.goertzen@gmail.com>
2024-09-30 17:29:28 -0700

    Add navbar and Home, Create, and Reports pages

---
+++ src/Create/Create.tsx
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+type CreateProps = {
+  //
+};
+
+class Create extends Component<CreateProps, any> {
+  public render() {
+    return <div>Create</div>;
+  }
+}
+
+export default Create;
---
+++ src/Reports/Reports.tsx
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+type ReportsProps = {
+  //
+};
+
+class Reports extends Component<ReportsProps, any> {
+  public render() {
+    return <div>Reports</div>;
+  }
+}
+
+export default Reports;
