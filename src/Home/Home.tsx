40f2d4a0cfe1ee3476a5e617a4c87d0a172f9b6c
chrisheesh <ceverett1@csuchico.edu>
2024-10-20 17:45:09 -0700

    closes #12: missing character set (UTF-8)

--- src/Home/Home.tsx
+++ src/Home/Home.tsx
@@ -92,7 +92,14 @@ const Home = ({}: HomeProps) => {
     <><div>
         
         <h1>Moods Calendar for My GoodDays</h1>
-        <Calendar/>
+        <Calendar
+        
+            //prevLabel="Prev Month"   // customize previous month button
+            //nextLabel="Next Month"   // customize next month button
+            //prev2Label="Prev Year"   // customize previous year button
+            //next2Label="Next Year"   // customize next year button
+        
+        />
         
         </div><br /><div>
             
